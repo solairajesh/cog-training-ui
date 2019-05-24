94ebde6879468a284b7f37fbaa37c40b49c493f2
solairajesh <solairajesh@gmail.com>
2019-05-24 21:00:10 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ import './style.css';
 // const appDiv = document.getElementById('app');
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
 // alert(123);
+
+var ordersData = (() => {
+
+             function getOrders(){
+               return fetch('https://jsonblob.com/api/4933fecc-7bd7-11e9-b226-2343c40bf0cc')
+                  .then(function(response) {
+                    return response.json();
+                  });
+             }
+
+            return {
+                getOrders: getOrders
+            };
+
+        })(); 
+ordersData.getOrders().then(function(response){
+
+console.log(response)
+
+}) 
